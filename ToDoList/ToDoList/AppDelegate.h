71c21ed9a078124fa15da10bcc67fadcb18265b0
rbnquintero <rbnquintero@Rubens-MacBook-Pro.local>
2015-08-10 13:10:29 -0500

    initial commit

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Ruben Quintero on 8/10/15.
+//  Copyright (c) 2015 Ruben Quintero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Ruben Quintero on 8/10/15.
+//  Copyright (c) 2015 Ruben Quintero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
